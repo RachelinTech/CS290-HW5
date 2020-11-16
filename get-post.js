db7b418919d83d6ac579ea894c36c1b77a6cdbff
Rachel Gonzalez <rachelintech@gmail.com>
2020-11-15 20:32:20 -0600

    Update get-post.js
    port changed from 3376 to 3925

--- get-post.js
+++ get-post.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
-app.set("port", 3376);
+app.set("port", 3925);
 
 app.get("/",function(req,res){
 	res.render("get_home");
